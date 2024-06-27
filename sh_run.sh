13c7091d7c631a1f465782593d203c3b33d3fc26
Shunsuke A. Sato <shunsuke.sato26@gmail.com>
2024-06-27 14:14:03 +0900

    Fixed a bug.

--- sh_run.sh
+++ sh_run.sh
@@ -1,35 +1,35 @@
 # Born
 echo 'Born'
-cp inp_born_eq > inp
+cp inp_born_eq inp
 ./test < inp > log_born_eq.log
 
 cp pop_t_born.out pop_t_born_eq.out
 
-cp inp_born > inp
+cp inp_born inp
 ./test < inp > log_born.log
 
 # Redfield
 echo 'Redfield'
-cp inp_redfield_eq > inp
+cp inp_redfield_eq inp
 ./test < inp > log_redfield_eq.log
 
 cp pop_t_redfield.out pop_t_redfield_eq.out
 
-cp inp_redfield > inp
+cp inp_redfield inp
 ./test < inp > log_redfield.log
 
 # Lindblad
 echo 'Lindblad'
-cp inp_lindblad_eq > inp
+cp inp_lindblad_eq inp
 ./test < inp > log_lindblad_eq.log
 
 cp pop_t_lindblad.out pop_t_lindblad_eq.out
 
-cp inp_lindblad > inp
+cp inp_lindblad inp
 ./test < inp > log_lindblad.log
 
 
 # Analysis
 echo 'Analysis'
-cp inp_analysis > inp
+cp inp_analysis inp
 ./test < inp > log_analysis.log
